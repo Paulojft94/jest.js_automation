6afc9f42897e673a651c4b5ed364e6bf6e15b0b4
Paulojft94 <1222013@isep.ipp.pt>
2023-03-15 22:27:03 +0000

    add ex14 lab02

---
+++ src/__tests__/lab02/ex14.test.ts
@@ -0,0 +1,38 @@
+import { empresaGrupo } from "../../lab02/functions02";
+
+describe("Validate the suspended comapnies", () => {
+  test("0 should return Indíce de poluícao aceitavel", () => {
+    const index: number = 0;
+    expect(empresaGrupo(index)).toBe("Índice de poluícão aceitável");
+  });
+
+  test("0.3 should return Indíce de poluícao aceitavel", () => {
+    const index: number = 0.3;
+    expect(empresaGrupo(index)).toBe("Índice de poluícão aceitável");
+  });
+
+  test("0.31 should return Indíce de poluícao aceitavel", () => {
+    const index: number = 0.31;
+    expect(empresaGrupo(index)).toBe("O grupo 1 está suspenso");
+  });
+
+  test("0.4 should return O grupo 1 está suspenso", () => {
+    const index: number = 0.4;
+    expect(empresaGrupo(index)).toBe("O grupo 1 está suspenso");
+  });
+
+  test("0.41 should return Indíce de poluícao aceitavel", () => {
+    const index: number = 0.41;
+    expect(empresaGrupo(index)).toBe("Os grupos 1 e 2 estão suspensos");
+  });
+
+  test("0.5 should return Indíce de poluícao aceitavel", () => {
+    const index: number = 0.5;
+    expect(empresaGrupo(index)).toBe("Os grupos 1 e 2 estão suspensos");
+  });
+
+  test("0.51 should return Indíce de poluícao aceitavel", () => {
+    const index: number = 0.51;
+    expect(empresaGrupo(index)).toBe("Todos os grupos estão suspensos");
+  });
+});
